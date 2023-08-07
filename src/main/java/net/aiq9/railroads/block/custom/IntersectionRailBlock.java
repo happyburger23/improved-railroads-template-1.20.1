58c6006672cd7426deea5a372d47521a9d1ebc57
aiq-9 <alexkaufman78@gmail.com>
2023-08-06 21:24:30 -0400

    Began working on track behavior. Hopefully won't have to use mixins if it goes well.

--- src/main/java/net/aiq9/railroads/block/custom/IntersectionRailBlock.java
+++ src/main/java/net/aiq9/railroads/block/custom/IntersectionRailBlock.java
@@ -1,5 +1,6 @@
 package net.aiq9.railroads.block.custom;
 
+import net.aiq9.railroads.mixin.AbstractMinecartEntityMixin;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.RailBlock;
@@ -9,6 +10,8 @@ import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.state.property.Property;
 import net.minecraft.util.BlockRotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class IntersectionRailBlock extends RailBlock {
     public static EnumProperty<RailShape> SHAPE;
@@ -16,6 +19,27 @@ public class IntersectionRailBlock extends RailBlock {
     public IntersectionRailBlock(Settings settings) {
         super(settings);
         this.setDefaultState(((this.stateManager.getDefaultState()).with(SHAPE, RailShape.NORTH_SOUTH)).with(WATERLOGGED, false));
+    }
+
+    @Override //do not update curves
+    protected BlockState updateCurves(BlockState state, World world, BlockPos pos, boolean notify) {
+        return null;
+    }
+
+    @Override //cannot create curves
+    public boolean cannotMakeCurves() {
+        return true;
+    }
+
+    @Override
+    public void updateBlockState(BlockState state, World world, BlockPos pos, Block neighbor) {
+        /*
+        if (world.isClient) {
+            world.setBlockState(pos, state.with(RailShape.), Block.NOTIFY_ALL);
+            world.updateNeighborsAlways(pos.down(), this);
+        }
+        */
+        super.updateBlockState(state, world, pos, neighbor);
     }
 
     @Override
