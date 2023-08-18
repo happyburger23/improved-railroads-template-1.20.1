e4677879ea9917940957f32aafbe9c202023c6c1
aiq-9 <alexkaufman78@gmail.com>
2023-08-18 14:24:54 -0400

    Fixed IntersectionRailBlock issue again?

--- src/main/java/net/aiq9/railroads/block/custom/IntersectionRailBlock.java
+++ src/main/java/net/aiq9/railroads/block/custom/IntersectionRailBlock.java
@@ -4,17 +4,13 @@ import net.minecraft.block.*;
 import net.minecraft.block.enums.RailShape;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Property;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -37,16 +33,6 @@ public class IntersectionRailBlock extends RailBlock {
         tooltip.add(Text.literal("Allows two at-grade rail lines to cross one another.").formatted(Formatting.GRAY));
         tooltip.add(Text.literal("FUTURE FEATURE").formatted(Formatting.RED));
         super.appendTooltip(stack, world, tooltip, options);
-    }
-
-    //possibly use this to create a simple way to change between blockstates?
-    @Override
-    public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
-        if (world.isClient) {
-            return;
-        }
-
-        super.onEntityCollision(state, world, pos, entity);
     }
 
     @Override
