4febef5e5829ef8037d60396db14f5472915eedc
aiq-9 <alexkaufman78@gmail.com>
2023-08-18 14:23:54 -0400

    Fixed minor issue. Continue work on IntersectionRailBlock

--- src/main/java/net/aiq9/railroads/block/custom/IntersectionRailBlock.java
+++ src/main/java/net/aiq9/railroads/block/custom/IntersectionRailBlock.java
@@ -4,18 +4,22 @@ import net.minecraft.block.*;
 import net.minecraft.block.enums.RailShape;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Property;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public class IntersectionRailBlock extends AbstractRailBlock {
+public class IntersectionRailBlock extends RailBlock {
     public static final EnumProperty<RailShape> SHAPE = EnumProperty.of("shape", RailShape.class, shape ->
             shape != RailShape.ASCENDING_NORTH &&
                     shape != RailShape.ASCENDING_EAST && shape != RailShape.ASCENDING_SOUTH && shape != RailShape.ASCENDING_WEST &&
@@ -23,7 +27,7 @@ public class IntersectionRailBlock extends AbstractRailBlock {
                     shape != RailShape.SOUTH_EAST && shape != RailShape.SOUTH_WEST);
 
     public IntersectionRailBlock(Settings settings) {
-        super(true, Settings.copy(Blocks.RAIL));
+        super(settings);
         setDefaultState(getDefaultState().with(SHAPE, RailShape.NORTH_SOUTH).with(WATERLOGGED, false));
     }
 
@@ -33,6 +37,16 @@ public class IntersectionRailBlock extends AbstractRailBlock {
         tooltip.add(Text.literal("Allows two at-grade rail lines to cross one another.").formatted(Formatting.GRAY));
         tooltip.add(Text.literal("FUTURE FEATURE").formatted(Formatting.RED));
         super.appendTooltip(stack, world, tooltip, options);
+    }
+
+    //possibly use this to create a simple way to change between blockstates?
+    @Override
+    public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
+        if (world.isClient) {
+            return;
+        }
+
+        super.onEntityCollision(state, world, pos, entity);
     }
 
     @Override
