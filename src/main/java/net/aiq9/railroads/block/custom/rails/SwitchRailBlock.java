ef969d15ba857e93cd37fef4b79c7d0f7f53d605
aiq-9 <alexkaufman78@gmail.com>
2023-09-21 18:56:21 -0400

    SwitchRailBlock removals and one re-addition

--- src/main/java/net/aiq9/railroads/block/custom/rails/SwitchRailBlock.java
+++ src/main/java/net/aiq9/railroads/block/custom/rails/SwitchRailBlock.java
@@ -25,7 +25,8 @@ import java.util.List;
 
 public class SwitchRailBlock extends PoweredRailBlock {
     public static final EnumProperty<RailShape> SHAPE = EnumProperty.of("shape", RailShape.class, shape ->
-            shape != RailShape.ASCENDING_NORTH && shape != RailShape.ASCENDING_EAST && shape != RailShape.ASCENDING_SOUTH && shape != RailShape.ASCENDING_WEST);
+            shape != RailShape.ASCENDING_NORTH && shape != RailShape.ASCENDING_EAST && shape != RailShape.ASCENDING_SOUTH && shape != RailShape.ASCENDING_WEST &&
+                    shape != RailShape.NORTH_EAST && shape != RailShape.NORTH_WEST && shape != RailShape.SOUTH_EAST && shape != RailShape.SOUTH_WEST);
 
     public static final DirectionProperty FACING;
     public static final BooleanProperty FLIPPED;
@@ -44,69 +45,22 @@ public class SwitchRailBlock extends PoweredRailBlock {
     }
 
     @Override
-    public boolean cannotMakeCurves() {
-        return true;
-    }
-
-    @Override
     public BlockState rotate(BlockState state, BlockRotation rotation) {
         switch (rotation) {
-            case CLOCKWISE_180: {
-                switch (state.get(SHAPE)) {
-                    case SOUTH_EAST -> state.with(SHAPE, RailShape.NORTH_WEST);
-                    case SOUTH_WEST -> state.with(SHAPE, RailShape.NORTH_EAST);
-                    case NORTH_WEST -> state.with(SHAPE, RailShape.SOUTH_EAST);
-                    case NORTH_EAST -> state.with(SHAPE, RailShape.SOUTH_WEST);
-                }
-            }
             case COUNTERCLOCKWISE_90: {
                 switch (state.get(SHAPE)) {
                     case NORTH_SOUTH -> state.with(SHAPE, RailShape.EAST_WEST);
                     case EAST_WEST -> state.with(SHAPE, RailShape.NORTH_SOUTH);
-                    case SOUTH_EAST -> state.with(SHAPE, RailShape.NORTH_EAST);
-                    case SOUTH_WEST -> state.with(SHAPE, RailShape.SOUTH_EAST);
-                    case NORTH_WEST -> state.with(SHAPE, RailShape.SOUTH_WEST);
-                    case NORTH_EAST -> state.with(SHAPE, RailShape.NORTH_WEST);
                 }
             }
             case CLOCKWISE_90: {
                 switch (state.get(SHAPE)) {
                     case NORTH_SOUTH -> state.with(SHAPE, RailShape.EAST_WEST);
                     case EAST_WEST -> state.with(SHAPE, RailShape.NORTH_SOUTH);
-                    case SOUTH_EAST -> state.with(SHAPE, RailShape.SOUTH_WEST);
-                    case SOUTH_WEST -> state.with(SHAPE, RailShape.NORTH_WEST);
-                    case NORTH_WEST -> state.with(SHAPE, RailShape.NORTH_EAST);
-                    case NORTH_EAST -> state.with(SHAPE, RailShape.SOUTH_EAST);
                 }
             }
         }
         return state;
-    }
-
-    @Override
-    public BlockState mirror(BlockState state, BlockMirror mirror) {
-        RailShape railShape = state.get(SHAPE);
-        switch (mirror) {
-            case LEFT_RIGHT: {
-                switch (railShape) {
-                    case SOUTH_EAST -> state.with(SHAPE, RailShape.NORTH_EAST);
-                    case SOUTH_WEST -> state.with(SHAPE, RailShape.NORTH_WEST);
-                    case NORTH_WEST -> state.with(SHAPE, RailShape.SOUTH_WEST);
-                    case NORTH_EAST -> state.with(SHAPE, RailShape.SOUTH_EAST);
-                }
-                break;
-            }
-            case FRONT_BACK: {
-                switch (railShape) {
-                    case SOUTH_EAST -> state.with(SHAPE, RailShape.SOUTH_WEST);
-                    case SOUTH_WEST -> state.with(SHAPE, RailShape.SOUTH_EAST);
-                    case NORTH_WEST -> state.with(SHAPE, RailShape.NORTH_EAST);
-                    case NORTH_EAST -> state.with(SHAPE, RailShape.NORTH_WEST);
-                }
-                break;
-            }
-        }
-        return super.mirror(state, mirror);
     }
 
     //plays piston_extend sound and will eventually flip the blockstate when clicked
